db38a28246c1ae49e8760652ac45086d97935432
Manish Bhattarai <manish.ilam@gmail.com>
2014-12-16 19:23:02 +0400

    updated the interface

--- WPS Generator/src/smartsewa/wps/Main.java
+++ WPS Generator/src/smartsewa/wps/Main.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -17,10 +18,13 @@ public class Main extends Application {
 						
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1000,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("WPS Generator 0.1");
 			primaryStage.show();
+			primaryStage.getIcons().add(new Image("icon.png"));
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
